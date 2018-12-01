5046d3756e1b9c16063d4662f855e5bd74af94bc
subashpenneru <subashpenneru@gmail.com>
2018-12-01 18:05:29 +0530

    mlab used

--- app/config/config.js
+++ app/config/config.js
@@ -1,11 +1,12 @@
 var host = "127.0.0.1"; 
 var port = 3030;
 
-const dbUrl = 'mongodb://127.0.0.1:27017/bot';
+// const dbUrl = 'mongodb://127.0.0.1:27017/bot';
+const dbUrl = "mongodb://hotel:subash1995@ds117334.mlab.com:17334/bot"
 
-const authSource = 'admin';
-const dbUser = 'rootuser';
-const dbPass = 'root';
+const authSource = 'bot';
+const dbUser = 'hotel';
+const dbPass = 'subash1995';
 const secretekey = "ThisisSubashPenneru"
 
 module.exports = {
