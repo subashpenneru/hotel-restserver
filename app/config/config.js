8215e606e9625cb1b87276bfdc1d006aa57e4c9f
Subash Penneru <subashpenneru@gmail.com>
2021-12-22 14:48:59 +0530

    config updated

--- app/config/config.js
+++ app/config/config.js
@@ -3,6 +3,7 @@ var port = 3030;
 
 // const dbUrl = 'mongodb://127.0.0.1:27017/bot';
 const dbUrl =
+  process.env.MONGO_URI ||
   'mongodb+srv://rootuser:root@mlab.sjox9.mongodb.net/bot?retryWrites=true&w=majority';
 
 const authSource = 'bot';
@@ -13,8 +14,8 @@ const userName = 'subashpenneru@gmail.com';
 const password = 'nani1995subash';
 
 module.exports = {
-  HOST: host,
-  PORT: port,
+  HOST: process.env.HOST || host,
+  PORT: process.env.PORT || port,
   DBURL: dbUrl,
   AUTHSOURCE: authSource,
   DBUSER: dbUser,
