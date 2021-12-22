b82d153f3f5a9a7c08a895876f7b794ca2272baf
Subash Penneru <subashpenneru@gmail.com>
2021-12-22 15:10:45 +0530

    config issues

--- app/config/config.js
+++ app/config/config.js
@@ -1,10 +1,7 @@
 var host = '127.0.0.1';
-var port = 3030;
 
 // const dbUrl = 'mongodb://127.0.0.1:27017/bot';
-const dbUrl =
-  process.env.MONGO_URI ||
-  'mongodb+srv://rootuser:root@mlab.sjox9.mongodb.net/bot?retryWrites=true&w=majority';
+const dbUrl = process.env.MONGO_URI;
 
 const authSource = 'bot';
 const dbUser = 'rootuser';
@@ -14,8 +11,8 @@ const userName = 'subashpenneru@gmail.com';
 const password = 'nani1995subash';
 
 module.exports = {
-  HOST: process.env.HOST || host,
-  PORT: process.env.PORT || port,
+  HOST: host,
+  PORT: process.env.PORT,
   DBURL: dbUrl,
   AUTHSOURCE: authSource,
   DBUSER: dbUser,
